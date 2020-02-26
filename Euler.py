ff71f6cd956e73c5d55915853a2eb71f354317f3
mikeonator <maudinyc@gmail.com>
2020-02-26 09:12:18 -0500

    e

--- Euler.py
+++ Euler.py
@@ -2,6 +2,7 @@
 ## Math Modeling
 
 from decimal import Decimal
+import matplotlib.pyplot as plt
 
 def main():
     x = 0
@@ -15,6 +16,7 @@ def main():
     
     xlist = [x]
     ylist = [y]
+    steplist = []
     
     for k in range(start, (end+1)):
         yprime = (2*x*y) - (3*(y**2)) + (2*x)
@@ -24,8 +26,10 @@ def main():
         y = y + deltay
         x = x + deltax
 
+        steplist.append(k)
         xlist.append(x)
         ylist.append(y)
+
         
         #scinotx = x     #'%.2E' % Decimal(str(x))
         #scinoty = y     #'%.2E' % Decimal(str(y))
@@ -33,6 +37,10 @@ def main():
         #print("[ " + str(scinotx) + " ] | [ " + str(scinoty) + " ]")
         #print("="*7 + "="*(len(str(scinotx))) + "="*(len(str(scinoty))))
 
+    biglist = [steplist, xlist, ylist]
+
+    for a in range(0, len(steplist)):
+        print(str(biglist[0][a]) + " ][ " + str(biglist[1][a]) + " ][ " + str(biglist[2][a]))
 
 if __name__ == '__main__':
     main()
