81a570a92735d772a3b1756d3657935f12299729
mikeonator <maudinyc@gmail.com>
2020-03-02 08:48:17 -0500

    Fibonacci Less than 10000

--- Fibonacci.py
+++ Fibonacci.py
@@ -2,8 +2,12 @@
 ## Mathematical Modeling - Mr. Sabree
 def main():
     fibList = [1,1]                                                                                                             #List with first two digits of sequence
-    for a in range(0,98):                                                                                                       #For loop, duh.
+    while(fibList[-1] < 10000):
         fibList.append(fibList[-2] + fibList[-1])                                                                               #Append the sum of the last two digits of the list to the list
-    print("\n" + 8*"_" + "\n\n" + "34th: " + str(fibList[34]) + "\n" + "78th: " + str(fibList[77]) + "\n" + 8*"_" + "\n")       #Print everything looking nice
+    
+    print(fibList[-2])                                                                                                          #Print the lsatnumber of the sequence less than 10000
+    
+    #print("\n" + 8*"_" + "\n\n" + "34th: " + str(fibList[34]) + "\n" + "78th: " + str(fibList[77]) + "\n" + 8*"_" + "\n")       #Print 35th and 78th numbers while looking nice
+
 if __name__ == '__main__':                                                                                                      #This thing basically lets the interpreter thing read my code before I run it all the way through because of how Python works
     main()
