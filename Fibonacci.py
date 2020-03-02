cb621a3e9d70e654a8c32028beae07a25db68191
mikeonator <maudinyc@gmail.com>
2020-03-02 08:51:43 -0500

    updated Comments

--- Fibonacci.py
+++ Fibonacci.py
@@ -2,10 +2,11 @@
 ## Mathematical Modeling - Mr. Sabree
 def main():
     fibList = [1,1]                                                                                                             #List with first two digits of sequence
+    #for a in range(0,98):                                                                                                       #for loop from 0-98 because the first two digits are already there
     while(fibList[-1] < 10000):
         fibList.append(fibList[-2] + fibList[-1])                                                                               #Append the sum of the last two digits of the list to the list
     
-    print(fibList[-2])                                                                                                          #Print the lsatnumber of the sequence less than 10000
+    print(fibList[-2])                                                                                                          #Print the last number of the sequence less than 10000
     
     #print("\n" + 8*"_" + "\n\n" + "34th: " + str(fibList[34]) + "\n" + "78th: " + str(fibList[77]) + "\n" + 8*"_" + "\n")       #Print 35th and 78th numbers while looking nice
 
