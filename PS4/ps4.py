b3ad9d6314a95a5715cceadeb17ff4a0cbb99a9d
mikeonator <maudinyc@gmail.com>
2020-04-19 14:08:22 -0400

    Added PS4

---
+++ PS4/ps4.py
@@ -0,0 +1,16 @@
+## Michael Audi 2020
+## Mathematical Modeling - Mr. Sabree
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from math import e
+
+def main():
+    problemOne()
+
+def problemOne():
+    print("e")
+
+
+
+if __name__ == '__main__':
+    main()
